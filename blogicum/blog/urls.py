5c5e4c33aa1a2b2fdc2c4fddf5afb4435cd9b890
Игорь Денисов <igorkennys228@mail.ru>
2023-05-21 23:48:05 +0500

    sprint77

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -10,12 +10,11 @@ urlpatterns = [
     path('posts/<int:pk>/', views.PostDetailView.as_view(),
          name='post_detail'),
     path('posts/create/', views.PostCreateView.as_view(), name='create_post'),
-    path('posts/<int:pk>/edit/', login_required(
-     views.PostUpdateView.as_view()),
-     name='edit_post'),
-    path('posts/<int:pk>/delete/', login_required(
-     views.PostDeleteView.as_view()),
-     name='delete_post'),
+    path('posts/<int:pk>/edit/', 
+         login_required(views.PostUpdateView.as_view()),
+         name='edit_post'),
+    path('posts/<int:pk>/delete/', 
+         login_required(views.PostDeleteView.as_view()), name='delete_post'),
     path('category/<slug:category_slug>/', views.CategoryListView.as_view(),
          name='category_posts'),
     path('profile/<slug:username>/',
@@ -23,13 +22,12 @@ urlpatterns = [
     path('edit_profile/', login_required(
      views.ProfileUpdateView.as_view()),
      name='edit_profile'),
-    path('posts/<int:pk>/comment/', login_required(
-     views.CommentAddView.as_view()),
-     name='add_comment'),
-    path('posts/<int:pk>/edit_comment/<int:id>/', login_required(
-     views.CommentUpdateView.as_view()),
-     name='edit_comment'),
-    path('posts/<int:pk>/delete_comment/<int:id>/', login_required(
-     views.CommentDeleteView.as_view()),
-     name='delete_comment'),
+    path('posts/<int:pk>/comment/',
+         login_required(views.CommentAddView.as_view()), name='add_comment'),
+    path('posts/<int:pk>/edit_comment/<int:id>/',
+         login_required(views.CommentUpdateView.as_view()),
+         name='edit_comment'),
+    path('posts/<int:pk>/delete_comment/<int:id>/',
+         login_required(views.CommentDeleteView.as_view()),
+         name='delete_comment'),
 ]
